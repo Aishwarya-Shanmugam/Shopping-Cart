d8ad5d9a5ca407b0719aff3a11f32b16075ae711
Aishwarya S <101408286+Aishwarya-Shanmugam@users.noreply.github.com>
2024-02-24 14:10:50 +0530

    Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div>
              <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright © Your Website 2023</p></div>
+            <div class="container"><p class="m-0 text-center text-white">Copyrights © SAV SHOPPING MART 2024</p></div>
             </footer>
         </div>
     );
